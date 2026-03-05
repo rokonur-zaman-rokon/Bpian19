b586b8a5a08b7119b76b4292777df845598f8441
rokonur zaman rokon <rrokonurzaman@gmail.com>
2026-03-05 14:05:34 +0600

    fix

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,3 @@
-import { createClient } from '@supabase/supabase-js'
-
 const supabaseUrl = 'https://eenlpvlwyoxajlcikmzi.supabase.co';
 const supabaseAnonKey = 'sb_publishable_9arPz9Gw94ymewnxj7rzbA_S6F5DxEE';
 
