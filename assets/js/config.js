f3bf5c34affc4f4983fd79b61b850e4929c57253
rokonur zaman rokon <rrokonurzaman@gmail.com>
2026-03-08 19:21:55 +0600

    FIX

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,4 @@
 const supabaseUrl = "https://eenlpvlwyoxajlcikmzi.supabase.co";
 const supabaseKey = "sb_publishable_9arPz9Gw94ymewnxj7rzbA_S6F5DxEE";
 
-const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
