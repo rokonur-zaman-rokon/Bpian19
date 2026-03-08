9d4785a0287d47f2e1b359ef2aee3e2d470b9f0e
rokonur zaman rokon <rrokonurzaman@gmail.com>
2026-03-08 19:28:18 +0600

    tt

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,6 @@
 const supabaseUrl = "https://eenlpvlwyoxajlcikmzi.supabase.co";
 const supabaseKey = "sb_publishable_9arPz9Gw94ymewnxj7rzbA_S6F5DxEE";
 
-const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+const { createClient } = supabase;
+
+const supabaseClient = createClient(supabaseUrl, supabaseKey);
